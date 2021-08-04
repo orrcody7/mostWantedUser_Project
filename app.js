d04bcee5dab1eb6fdb27d8b69fb71eb05ba82a27
unknown <orrcody7@gmail.com>
2021-08-04 14:25:05 -0600

    fixed bug on line 136 from !== to ===

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ function promptFor(question, valid){
   do{
     response = prompt(question).trim();
     isValid = valid(response);
-  } while(response !== ""  ||  isValid === false)
+  } while(response === ""  ||  isValid === false)
   return response
 }
 
